23e6d67422f8d4859fed8a565e786057689f5de1
Brandon "Alexis" Bleau <alexis@bleauweb.net>
2015-09-01 11:24:29 -0400

    Update Dialer
    Added stubs for future work, and made some of the functions only accessible from inside the script.

--- LanteaCraft/dialer.lua
+++ LanteaCraft/dialer.lua
@@ -6,18 +6,34 @@
 
 local rednetWired = 'back'
 local rednetWireless = 'front'
+local stargateName = 'stargate_0'
 
 rednet.open(rednetWired)
 rednet.open(redneetWireless)
+local stargate = peripheral.wrap(stargateName)
 
 -- Checks the make sure the address passes validation.
-function check_address(address)
-  print('SGC: Stargate address failed validation')
+local function check_address(address)
+  print('SGC: Unrecognized address or address configuration!')
+  return false
+end
+
+local function check_status()
+  print('SGC: Unauthorized stargate activity!')
   return false
 end
 
 -- Checks the address to make sure it can dial without crashing
 -- the script. 
-function stargate_dial(address)
+function stargate_connect(address)
   local addressCheck = check_address(address)
+  if addressCheck then
+    stargate.connect(address)
+  end
+end
+
+function stargate_disconnect()
+  if check_state() then
+    stargate.disconnect()
+  end
 end
