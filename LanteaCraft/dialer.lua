3e253ddfc3c75052b5e49ade1b372b655bb8d1f3
Brandon "Alexis" Bleau <alexis@bleauweb.net>
2015-09-01 13:53:48 -0400

    Update is_in_address_database function in Dialer

--- LanteaCraft/dialer.lua
+++ LanteaCraft/dialer.lua
@@ -14,15 +14,54 @@ rednet.open(rednetWired)
 rednet.open(redneetWireless)
 sgPeripheral = peripheral.wrap(stargateName)
 
+-- global data storage (yuck)
+addressLookupReturn = ''
+keepAddress = ''
+
 -- Error Message Strings
 error_unrecognized_address = 'SGC: Unrecognized address or address configuration!'
 error_not_yet_implimented = 'Error: Not Yet Implimented!'
 
+-- waits for three seconds and then returns true.
+local function wait_three_seconds()
+  i = 1
+  while i >= 2 do
+    sleep(1)
+    i = i + 1
+  end
+  
+  return true
+end
+
+local function read_address_lookup()
+  local t = {};i=1
+  t[1], t[2], t[3] = rednet.reseave('addressLookupReturn')
+  addressLookupReturn = t
+  return true
+end
+
 -- Looks the address up on the Stargate Address Computer
 -- works for any addresses and aliases
 local function is_in_address_database(address)
-  print(error_not_yet_implimented)
-  return false
+  -- send the address to the address database computer
+  rednet.broadcast('addressLookup', address) 
+  
+  -- wait to retreive the message, but no longer then three seconds
+  parallel.waitForAny(read_address_lookup, wait_three_seconds)
+  
+  -- save the addressLookupReturn locally
+  local addressLookupTable = addressLookupReturn 
+  
+  -- clear out global storage
+  addressLookupReturn = '' 
+  
+  -- check to see if the address was returned
+  if not addressLookupTable[2] == 'false' then
+    keepAddress = addressLookupTable[2]
+    return true
+  else
+    return false
+  end
 end
 
 -- Checks the make sure the address passes validation.
@@ -43,8 +82,18 @@ end
 
 -- checks to see if the gate is in a spacific state.
 local function is_in_state(state)
-  print(error_not_yet_implimented)
-  return false
+  local currentState = ''
+  if sgPeripheral.isDialing() then
+    currentState = 'active'
+  else
+    currentState = 'inactive'
+  end
+  
+  if currentState == state then
+    return true
+  else
+    return false
+  end
 end
 
 -- Checks the address to make sure it can dial without crashing
