298245bc5a35354b2a3e58d6eb1a869390265b90
Brandon "Alexis" Bleau <alexis@bleauweb.net>
2015-09-01 11:56:50 -0400

    Update Dialer
    Added a few more function stubs, still a work in progress. Addresses currently will always fail
    validation.

--- LanteaCraft/dialer.lua
+++ LanteaCraft/dialer.lua
@@ -12,10 +12,24 @@ rednet.open(rednetWired)
 rednet.open(redneetWireless)
 local stargate = peripheral.wrap(stargateName)
 
+-- Looks the address up on the Stargate Address Computer
+-- works for any addresses and aliases
+local function lookup_address(address)
+  return false
+end
+
 -- Checks the make sure the address passes validation.
 local function check_address(address)
-  print('SGC: Unrecognized address or address configuration!')
-  return false
+  local addressLength = string.len(address)
+  if address == nil then
+    print('SGC: Unrecognized address or address configuration!')
+    return false
+  elseif lookup_address(address) then
+    return true
+  else
+    print('SGC: Unrecognized address or address configuration!')
+    return false
+  end
 end
 
 local function check_status()
@@ -26,8 +40,7 @@ end
 -- Checks the address to make sure it can dial without crashing
 -- the script. 
 function stargate_connect(address)
-  local addressCheck = check_address(address)
-  if addressCheck then
+  if check_address(address) then
     stargate.connect(address)
   end
 end
